0792d9430da2ec6b224dd6010011ab50e672d392
Bayekula-hb <hobedbayekula@gmail.com>
2024-07-16 12:19:16 +0100

    Refactor: add the salt

--- src/seeding/seeding.service.ts
+++ src/seeding/seeding.service.ts
@@ -58,9 +58,10 @@ export class SeedingService {
 
       await roleRepository.save([adminRole, supportRole, managerRole, customerRole, supportEstablishmentRole]);
 
+      const userSalt = await bcrypt.genSalt();
       const adminUser = userRepository.create({
         username: process.env.ADMIN_MAIL,
-        password: await bcrypt.hash(process.env.ADMIN_PASSWORD),
+        password: await bcrypt.hash(process.env.ADMIN_PASSWORD, userSalt),
         email: process.env.ADMIN_MAIL,
         firstname: 'hobed',
         lastname: 'bayekula',
